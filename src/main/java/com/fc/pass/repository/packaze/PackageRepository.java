663ddfbaca85b897ea9efd59c272023c853a4f26
Kwoojin <dnwls488@naver.com>
2023-02-12 23:52:22 +0900

    Batch 테스트를 위한 기본 Init

---
+++ src/main/java/com/fc/pass/repository/packaze/PackageRepository.java
@@ -0,0 +1,6 @@
+package com.fc.pass.repository.packaze;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
+}
---
+++ src/test/java/com/fc/pass/repository/packaze/PackageRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.fc.pass.repository.packaze;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+//@ActiveProfiles("test")
+class PackageRepositoryTest {
+
+    @Autowired
+    private PackageRepository packageRepository;
+
+    @Rollback(value = false)
+    @Test
+    public void test_save() {
+        // given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("바디 챌린지 PT 12주");
+        packageEntity.setPeriod(84);
+
+        // when
+        packageRepository.save(packageEntity);
+
+        // then
+        assertThat(packageEntity.getPackageSeq()).isNotNull();
+
+    }
+
+}
